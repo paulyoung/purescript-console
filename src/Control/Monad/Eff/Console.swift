513b1f25cbb813d6b1f15bbc051beb19ddd6ae34
Paul Young <paulyoungonline@gmail.com>
2017-01-02 11:59:07 +0000

    Use `Any` instead of `CustomStringConvertible`
    The first argument to `print` has the type of `Any`, and not all printable types
    conform to `CustomStringConvertible` e.g. the metatype or instance of a struct.

--- src/Control/Monad/Eff/Console.swift
+++ src/Control/Monad/Eff/Console.swift
@@ -1,24 +1,24 @@
 // Control.Monad.Eff.Console
 
-public func log(_ s: CustomStringConvertible) -> () -> () {
+public func log(_ s: Any) -> () -> () {
     return { _ in
         return print("LOG:", s)
     }
 }
 
-public func warn(_ s: CustomStringConvertible) -> () -> () {
+public func warn(_ s: Any) -> () -> () {
     return { _ in
         return print("WARN:", s)
     }
 }
 
-public func error(_ s: CustomStringConvertible) -> () -> () {
+public func error(_ s: Any) -> () -> () {
     return { _ in
         return print("ERROR:", s)
     }
 }
 
-public func info(_ s: CustomStringConvertible) -> () -> () {
+public func info(_ s: Any) -> () -> () {
     return { _ in
         return print("INFO:", s)
     }
